6fcb75d15b2c70b0fd34237a16d2698ad6a7c841
laller2000 <69633175+laller2000@users.noreply.github.com>
2021-03-18 15:54:59 +0100

    1.0version
    1.0version

---
+++ kosar2004/kosar2004/Liga.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kosar2004
+{
+    class Liga
+    {
+        //hazai;idegen;hazai_pont;idegen_pont;helyszín;időpont
+        private string hazai;
+        private string idegen;
+        private int hazai_pont;
+        private int idegen_pont;
+        private string helyszin;
+        private DateTime idopont;
+
+        public string Hazai { get => hazai; set => hazai = value; }
+        public string Idegen { get => idegen; set => idegen = value; }
+        public int Hazai_pont { get => hazai_pont; set => hazai_pont = value; }
+        public int Idegen_pont { get => idegen_pont; set => idegen_pont = value; }
+        public string Helyszin { get => helyszin; set => helyszin = value; }
+        public DateTime Idopont { get => idopont; set => idopont = value; }
+
+        public Liga(string hazai, string idegen, int hazai_pont, int idegen_pont, string helyszin, DateTime idopont)
+        {
+            Hazai = hazai;
+            Idegen = idegen;
+            Hazai_pont = hazai_pont;
+            Idegen_pont = idegen_pont;
+            Helyszin = helyszin;
+            Idopont = idopont;
+        }
+        public Liga(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Hazai = adatok[0];
+            Idegen = adatok[1];
+            Hazai_pont = int.Parse(adatok[2]);
+            Idegen_pont = int.Parse(adatok[3]);
+            Helyszin = adatok[4];
+            Idopont = DateTime.Parse(adatok[5]);
+        }
+    }
+}
---
+++ kosar2004/kosar2004/kosar2004.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kosar2004
+{
+    class kosar2004
+    {
+        static List<Liga> liga = new List<Liga>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2.feladat:eredmenyek.csv beolvasása!");
+            Beolvas("eredmenyek.csv");
+            int hazaidb = 0;
+            int idegendb = 0;
+            for (int i = 0; i < liga.Count; i++)
+            {
+                if (liga[i].Hazai=="Real Madrid")
+                {
+                    hazaidb++;
+                }
+                else if(liga[i].Idegen=="Real Madrid")
+                {
+                    idegendb++;
+                }
+            }
+            Console.WriteLine($"\n3.feladat:Real Madrid: Hazai:{hazaidb} Idegen:{idegendb}");
+            string voltdontelten =" ";
+            for (int i = 0; i < liga.Count; i++)
+            {
+                if (liga[i].Hazai_pont==liga[i].Idegen_pont)
+                {
+                    voltdontelten = "Igen";
+                    return;
+                }
+                else
+                {
+                    voltdontelten = "Nem";
+                }
+
+            }
+           
+            Console.WriteLine($"\n4.feladat:Voltdontetlen?:{voltdontelten}");
+            for (int i = 0; i < liga.Count; i++)
+            {
+                if (liga[i].Idegen.Contains("Barcelona"))
+                {
+                    Console.WriteLine($"\n5.feladat:barcelonai csapat neve:{liga[i].Idegen}");
+                }
+            }
+            
+            Console.WriteLine("\n6.feladat:");
+            for (int i = 0; i < liga.Count; i++)
+            {
+                if (liga[i].Idopont.Year==2004 && liga[i].Idopont.Month==11 && liga[i].Idopont.Day==21)
+                {
+                    Console.WriteLine($"\t{liga[i].Hazai}-{liga[i].Idegen} ({liga[i].Hazai_pont}:{liga[i].Idegen_pont})");
+                }
+            }
+            Console.WriteLine("\n7.feladat:");
+            var stadionok = liga.GroupBy(a => a.Helyszin).Select(b => new { o = b.Key, db = b.Count() }).Where(c => c.db > 20);
+            foreach (var item in stadionok)
+            {
+                Console.WriteLine($"\t{item.o}:{item.db}");
+            }
+            Console.WriteLine("\nProgram Vége!");
+            Console.ReadLine();
+        }
+        static void Beolvas(string filenev)
+        {
+            using (StreamReader sr=new StreamReader(filenev,Encoding.Default))
+            {
+                string fejlec = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    Liga l = new Liga(sr.ReadLine());
+                    liga.Add(l);
+                }
+            }
+        }
+    }
+}
